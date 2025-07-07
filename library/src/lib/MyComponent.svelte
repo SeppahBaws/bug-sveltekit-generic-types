918dbbf920b2755a7dc8d439d97c80f1823aed79
Seppe Dekeyser <seppe.dekeyser@hotmail.com>
2025-07-07 10:30:00 +0200

    Create reproduction

---
+++ library/src/lib/MyComponent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" generics="MyType">
+    import type { Example } from "@repo/utility";
+
+    interface Props {
+        prop1: Example<MyType>;
+    }
+    let { prop1 }: Props = $props();
+</script>
+
+<p>Hello World</p>
